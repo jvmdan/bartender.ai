1677fa9f8fe0635d99a693a7d0325b2c7d7bc3d8
Daniel Scarfe <daniel.scarfe@icloud.com>
2023-03-07 13:37:04 +0000

    Created bootstrap frontend

--- src/main/java/uk/scarfe/cocktails/generator/Generator.java
+++ src/main/java/uk/scarfe/cocktails/generator/Generator.java
@@ -1,10 +1,8 @@
 package uk.scarfe.cocktails.generator;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 import uk.scarfe.cocktails.model.Ingredient;
 import uk.scarfe.cocktails.model.Recipe;
